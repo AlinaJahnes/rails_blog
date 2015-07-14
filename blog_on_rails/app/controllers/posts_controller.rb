58af77f71697df0c5475de4806d5730cec9770eb
AlinaJahnes <alina.jahnes@gmail.com>
2015-07-14 13:49:42 -0400

    allow user update posts

--- blog_on_rails/app/controllers/posts_controller.rb
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -8,6 +8,25 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to new_post_path, error: @post.errors.full_messages
+    end
+  end
 
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id]).update(post_params)
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content).merge(user_id: session[:id])
   end
 end
