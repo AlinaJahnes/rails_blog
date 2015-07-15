7f65fd6de408e406d641bf2aa8375d7aaa217535
AlinaJahnes <alina.jahnes@gamil.com>
2015-07-14 22:52:13 -0400

    move post helper method to application helper

--- blog_on_rails/app/helpers/application_helper.rb
+++ blog_on_rails/app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
     if session[:id]
       @current_user ||= User.find(session[:id])
     end
+  end	
+
+  def authorized?(post)
+    post.user_id == session[:id]
   end
 end
--- blog_on_rails/app/helpers/posts_helper.rb
+++ blog_on_rails/app/helpers/posts_helper.rb
@@ -1,5 +1,2 @@
 module PostsHelper
-  def authorized?(post)
-    post.user_id == session[:id]
-  end
 end
